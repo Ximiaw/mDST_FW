ce40e8b4774fa3ce014e35f8530e38e5c4604a24
Ximiaw <m3311764705@outlook.com>
2026-06-30 20:12:42 +0800

    refactor(character.lua): 重构character.lua以简化角色管理
    重构character.lua以简化角色管理，移除了旧的GENDER定义和相关函数，并引入新的角色数据结构和加载机制。
    
    - 移除了GENDER定义和character:new函数，因为它们不再符合新的设计架构。
    - 移除了character:AddRecipe、character:AddTag、character:AddTags、character:AddRecipeTab、character:RemapSoundEvent和character:AddLines函数，因为它们的功能被新的add和load函数替代。
    - 引入了一个新的字符数据结构，使用table.insert来管理角色数据。
    - 添加了character:add函数，用于添加新的角色数据。
    - 添加了character:load函数，用于加载和初始化角色数据，在modmain调用。

--- character.lua
+++ character.lua
@@ -1,62 +1,44 @@
-GENDER = {MALE="MALE",FEMALE="FEMALE",ROBOT="ROBOT",NEUTRAL="NEUTRAL",PLURAL="PLURAL",__type="GENDER"}
-character = {}
+--[[
+local character = {
+    data={
+        [1]={
+            name="",
+            gender="",
+            modes={},
+            tag={},
+            character_recipe={
+                private={},
+                public={}
+            },
+            recipe_tab={},
+            tech={},
+            prototyper={},
 
-function character:new(name, gender, modes)
-    if gender.__type ~= "GENDER" then
-        error("参数gender必须是GENDER类型，当前为："..type(gender))
-    end
-
-    local o = {
-        name=name,
-        gender=gender,
-        modes=modes
+        }
     }
+}
+]]
+
+
+
+
+
+
+local character = {
+    data={}
+}
+
+function character:add(...)
+    local o = {...}
+    table.insert(character.data,o)
     setmetatable(o,{__index=self})
-    AddModCharacter(name, gender, modes)
-    AddPrefabPostInit(name,name)
     return o
 end
 
--- 添加制作配方
-function character:AddRecipe(name, ingredients, tab, 
-    level, placer_or_more_data, min_spacing, 
-    nounlock, numtogive, 
-    atlas, image, testfn, 
-    product, build_mode, build_distance)
-
-    AddRecipe(name, ingredients, tab, 
-        level, placer_or_more_data, min_spacing, 
-        nounlock, numtogive, self.name, 
-        atlas, image, testfn, 
-        product, build_mode, build_distance)
+function character:load()
+    for key, value in pairs(character.data) do
+        -- todo:加载和初始化，在modmain调用
+    end
 end
 
-function character:AddTag(tag)
-    AddPrefabPostInit(self.name, function(inst)
-        inst:AddTag(tag)
-    end)
-end
-
--- 批量添加多个 tag
-function character:AddTags(...)
-    local tags = {...}
-    AddPrefabPostInit(self.name, function(inst)
-        for _, tag in ipairs(tags) do
-            inst:AddTag(tag)
-        end
-    end)
-end
-
--- 制作科技栏
-function character:AddRecipeTab(rec_str, rec_sort, rec_atlas, rec_icon, rec_crafting_station)
-    AddRecipeTab(rec_str, rec_sort, rec_atlas, rec_icon, self.name, rec_crafting_station)
-end
-
-function character:RemapSoundEvent(name, new_name)
-    RemapSoundEvent(name, new_name)
-end
-
--- 加载人物文本
-function character:AddLines(path)
-    modimport(path)
-end
+return character
