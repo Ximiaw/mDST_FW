a0a129e09adf9b476da854d1301bf68f615ddcdd
Ximiaw <m3311764705@outlook.com>
2026-06-30 20:37:14 +0800

    feat(character): 添加角色信息设置和标签操作功能
    - 封装 `new` 函数，用于初始化角色对象，保留原有的 `add` 函数以支持一次性配置的接口。
    - 修改 `load` 函数为 `load_game`，并添加注释说明其用途为在 `modmain` 调用时加载和初始化角色。
    - 添加 `set_info` 函数，用于设置角色的姓名、性别和模式。
    - 添加 `add_tag` 函数，用于为角色添加标签。
    - 添加 `rm_tag` 函数，用于移除角色的标签。
    
    这些更改增强了角色对象的功能性，提供了更灵活的配置和管理方式。

--- character.lua
+++ character.lua
@@ -20,25 +20,36 @@ local character = {
 ]]
 
 
-
-
-
-
 local character = {
     data={}
 }
 
-function character:add(...)
+-- 封装配置选项，使得更好配置，但要保留一次性配置的接口
+function character:new(...)
     local o = {...}
     table.insert(character.data,o)
     setmetatable(o,{__index=self})
     return o
 end
 
-function character:load()
+function character:load_game()
     for key, value in pairs(character.data) do
         -- todo:加载和初始化，在modmain调用
     end
+end
+
+function character:set_info(name,gender,modes)
+    self.name = name
+    self.gender = gender
+    self.modes = modes
+end
+
+function character:add_tag(tag)
+    self.tag[tag] = tag
+end
+
+function character:rm_tag(tag)
+    self.tag[tag] = nil
 end
 
 return character
